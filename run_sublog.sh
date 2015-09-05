d7f7b7672a382b2b76415923a9a5e20e126e9706
mikemey <miguel.maestro@gmx.co.uk>
2015-09-05 15:38:16 +0100

    cosmetic brush-up.

--- run_sublog.sh
+++ run_sublog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# screen -dmS sublog python manage.py runserver 0.0.0.0:4444
+# screen -dmS sublog /usr/bin/python manage.py runserver 0.0.0.0:4444
 python manage.py runserver 0.0.0.0:4444
