4ef4f6b33c9994711447f4a920376eea8b63dc80
mikemey <miguel.maestro@gmx.co.uk>
2015-09-05 02:42:34 +0100

    another attempt to get network interface config right.

--- sublog/settings.py
+++ sublog/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 't3oc2%b_5+_zs_ir(6&eivsfvj@6w^@%ag=f5-uji(w708ob(x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = (
