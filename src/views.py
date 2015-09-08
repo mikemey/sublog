e95d8718a7dfe69d902db401bcaa02e72bc6fd6d
mikemey <miguel.maestro@gmx.co.uk>
2015-09-08 15:41:02 +0100

    WIP: started to add bootstrap 3 design.
    - finshed index, article-detail (including comment sections) redesign.

--- src/views.py
+++ src/views.py
@@ -1,12 +1,17 @@
-from django.http.response import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
+
+from django.http.response import HttpResponseRedirect
+
+from django.shortcuts import render, get_object_or_404
+
 from django.db import transaction
 
 from django.views import generic
 
 from src import ARTICLES_VISIBLE
 from src.models import Article, ArticleComment
+
+MISSING_FIELDS_ERROR = 'Required field(s) missing: %s'
 
 
 class IndexView(generic.ListView):
@@ -31,22 +36,18 @@ class CreateArticleView(generic.CreateView):
 
 def comment(request, article_id):
     art = get_object_or_404(Article, pk=article_id)
-    try:
-        art_comment = ArticleComment(
-            article=art,
-            user_name=request.POST['name'],
-            user_email=request.POST['email'],
-            content=request.POST['content'],
-            title=request.POST.get('title', '')
-        )
-    except KeyError:
+
+    parsed_comment = parse_comment_post(art, request.POST)
+
+    if parsed_comment.error_message:
         return render(request, 'article.html', {
             'article': art,
-            'error_message': 'Please fill in all required fields marked with an asterisk.',
+            'form_data': parsed_comment.form_data,
+            'error_message': parsed_comment.error_message
         })
 
-    store_comment(art, art_comment)
-    return HttpResponseRedirect(reverse('article', args=(article_id,)))
+    store_comment(art, parsed_comment.comment)
+    return HttpResponseRedirect(reverse('article', args=(article_id,)) + '#comments')
 
 
 @transaction.atomic
@@ -54,3 +55,44 @@ def store_comment(art, article_comment):
     article_comment.save()
     art.comments_count = art.comments.count()
     art.save()
+
+
+def parse_comment_post(art, post_data):
+    missing = []
+    collected = {}
+    name = get_post_field(post_data, 'name', missing, collected)
+    email = get_post_field(post_data, 'email', missing, collected)
+    content = get_post_field(post_data, 'content', missing, collected)
+    title = get_post_field(post_data, 'title', missing, collected, False)
+
+    if missing:
+        error_message = MISSING_FIELDS_ERROR % ', '.join(missing)
+        return CommentResult(None, error_message, collected)
+
+    art_comment = ArticleComment(
+        article=art,
+        user_name=name,
+        user_email=email,
+        content=content,
+        title=title
+    )
+    return CommentResult(art_comment)
+
+
+def get_post_field(post_data, param, missing, collected, mandatory=True):
+    if param in post_data:
+        value = post_data.get(param)
+        collected[param] = value
+        if mandatory and (not value or value.isspace()):
+            missing.append(param)
+        return value
+
+    missing.append(param)
+    return None
+
+
+class CommentResult:
+    def __init__(self, cmt, error_message=None, form_data=None):
+        self.comment = cmt
+        self.error_message = error_message
+        self.form_data = form_data
