0ea7a282eba63915d5b79116e0850a2de584ea02
mikemey <miguel.maestro@gmx.co.uk>
2015-09-09 14:51:39 +0100

    reformatting.

--- sublog/request_logging.py
+++ sublog/request_logging.py
@@ -46,12 +46,14 @@ def post_data_from(request):
 
     return post_data
 
+
 def client_ip(request):
     ip_addr = request.META.get('HTTP_X_FORWARDED_FOR', UNKNOWN_STRING)
     if ip_addr is UNKNOWN_STRING:
         ip_addr = request.META.get('REMOTE_ADDR', UNKNOWN_STRING)
 
     return ip_addr
+
 
 def simple_user_agent(request):
     full_user_agent = request.META.get('HTTP_USER_AGENT', UNKNOWN_STRING)
