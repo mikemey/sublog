034feb7c4cba4cd56750f807a090282dfd514b03
mikemey <miguel.maestro@gmx.co.uk>
2015-09-09 11:04:51 +0100

    enhanced logging format.
    resolves #6

--- sublog/request_logging.py
+++ sublog/request_logging.py
@@ -1,14 +1,19 @@
 import logging
 import datetime
 
+from user_agents import parse
+
+UNKNOWN_STRING = 'NKN'
+
 
 class RequestLoggingMiddleware(object):
     def __init__(self):
         self.logger = logging.getLogger('sublog')
 
-    def log_message(self, method, path, response_status, time, post_msg=None):
-        msg = """%s "%s" %s (%sms) """ % \
-              (method, path, response_status, time)
+    def log_message(self, method, path, response_status, time, user_ip, user_agent, post_msg=None):
+        msg = """%s %s %s (%sms) [%s, '%s', '%s', '%s']""" % \
+              (method, path, response_status, time, user_ip,
+               user_agent[0], user_agent[1], user_agent[2])
 
         if post_msg:
             msg += ' post-msg: [%s]' % post_msg
@@ -27,6 +32,8 @@ class RequestLoggingMiddleware(object):
 
         self.log_message(method, path, status,
                          time.microseconds / 1000,
+                         request.META['REMOTE_ADDR'],
+                         simple_user_agent(request),
                          post_data_from(request))
         return response
 
@@ -38,3 +45,15 @@ def post_data_from(request):
         post_data = "title: '%s'" % post_data
 
     return post_data
+
+
+def simple_user_agent(request):
+    full_user_agent = request.META.get('HTTP_USER_AGENT', UNKNOWN_STRING)
+    if full_user_agent is not UNKNOWN_STRING:
+        user_agent = parse(full_user_agent)
+        os = user_agent.os.family
+        device = user_agent.device.family
+        browser = user_agent.browser.family
+        return os, device, browser
+
+    return UNKNOWN_STRING, UNKNOWN_STRING, UNKNOWN_STRING
