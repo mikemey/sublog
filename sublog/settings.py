c85eab042bc8b3ecd19d50faf3517bb86e16cc61
mikemey <miguel.maestro@gmx.co.uk>
2015-09-09 14:51:50 +0100

    static files moved to original folder.

--- sublog/settings.py
+++ sublog/settings.py
@@ -79,7 +79,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/share/Web'
+STATIC_ROOT = '/share/Web/static'
 
 LOGGING_CONFIG = None
 # from django.conf import global_settings
