0e7d4f2fb005ab53d79b3eff4e483bef5083c74e
mikemey <miguel.maestro@gmx.co.uk>
2015-09-18 09:48:11 +0100

    fixed draft test formatting issue (tests were excluded).

--- src/tests/test_draft.py
+++ src/tests/test_draft.py
@@ -10,33 +10,29 @@ class DraftTests(SublogTestCase):
         draft_response = self.get_draft()
         self.assertEquals(403, draft_response.status_code)
 
+    def test_post_draft_not_allowed_for_anon(self):
+        draft_response = self.post_draft(TITLE_1, CONTENT_1)
+        self.assertEquals(403, draft_response.status_code)
 
-def test_post_draft_not_allowed_for_anon(self):
-    draft_response = self.post_draft(TITLE_1, CONTENT_1)
-    self.assertEquals(403, draft_response.status_code)
+    def test_post_draft_is_stored(self):
+        DRAFT_CACHE.invalidate()
+        self.login()
+        self.assert_draft('', '')
+        draft_response = self.post_draft(TITLE_1, CONTENT_1)
+        self.assertEquals(201, draft_response.status_code)
 
+        self.assert_draft(TITLE_1, CONTENT_1)
 
-def test_post_draft_is_stored(self):
-    DRAFT_CACHE.invalidate()
-    self.login()
-    self.assert_draft('', '')
-    draft_response = self.post_draft(TITLE_1, CONTENT_1)
-    self.assertEquals(201, draft_response.status_code)
+    def test_post_draft_is_deleted_after_post_article(self):
+        DRAFT_CACHE.invalidate()
+        self.login()
+        self.post_draft(TITLE_1, CONTENT_1)
+        self.post_article(TITLE_2, CONTENT_2)
 
-    self.assert_draft(TITLE_1, CONTENT_1)
+        self.assert_draft('', '')
 
-
-def test_post_draft_is_deleted_after_post_article(self):
-    DRAFT_CACHE.invalidate()
-    self.login()
-    self.post_draft(TITLE_1, CONTENT_1)
-    self.post_article(TITLE_2, CONTENT_2)
-
-    self.assert_draft('', '')
-
-
-def assert_draft(self, title, content):
-    draft_response = self.get_draft()
-    data = json.loads(draft_response.content)
-    self.assertEquals(title, data.get('title'))
-    self.assertEquals(content, data.get('content'))
+    def assert_draft(self, title, content):
+        draft_response = self.get_draft()
+        data = json.loads(draft_response.content)
+        self.assertEquals(title, data.get('title'))
+        self.assertEquals(content, data.get('content'))
